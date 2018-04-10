7d2394a83500db90dee08a134c14636310488989
Cobbleopolis <cobbleopolis@gmail.com>
2018-04-10 15:31:10 -0400

    Began properly implementing the car controller

--- Assets/Scripts/Cobble/SpyHunter/Player/CarController.cs
+++ Assets/Scripts/Cobble/SpyHunter/Player/CarController.cs
@@ -3,23 +3,87 @@ using UnityEngine;
 
 namespace Cobble.SpyHunter.Player {
 	
-	[RequireComponent(typeof(Rigidbody))]
-	[RequireComponent(typeof(CharacterController))]
 	public class CarController : MonoBehaviour {
 
-		[Tooltip("The speed that the character moves at")]
-		public float Speed = 5;
+		[Serializable]
+		public class CarMovementSettings {
+			[Tooltip("The speed that the character moves at")]
+			public float MovementSpeed = 5.0f;
+
+			public float MaxSpeed = 25.0f;
+
+			public float CurrentSpeed;
+		
+			public float CurrentTargetSpeed;
+		}
+
+		[Serializable]
+		public class CarAdvancedSettings {
+			[Tooltip("The distance for checking if the controller is grounded (0.01f seems to work best for this)")]
+			public float GroundCheckDistance = 0.01f;
+			public LayerMask GroundCheckLayerMask = -1;
+			public float StickToGroundHelperDistance = 0.5f; // stops the character
+			[Tooltip("Can the user control the direction that is being moved in the air")]
+			public bool AirControl; // 
+			[Tooltip("Reduce the radius by that ratio to avoid getting stuck in wall. Set it to 0.1 or more if you get stuck in wall")]
+			public float ShellOffset;
+		}
+
+		public CarMovementSettings MovementSettings = new CarMovementSettings();
+		public CarAdvancedSettings AdvancedSettings = new CarAdvancedSettings();
 
 		[SerializeField]
-		private CharacterController _characterController;
+		private Rigidbody _rigidbody;
+
+		[SerializeField] 
+		private BoxCollider _boxCollider;
+		
+		private Vector3 _groundContactNormal;
+		private bool _isGrounded;
 
 		private void Start () {
-			if (!_characterController)
-				_characterController = GetComponent<CharacterController>();
+			if (!_rigidbody)
+				_rigidbody = GetComponentInChildren<Rigidbody>();
+			if (!_boxCollider)
+				_boxCollider = GetComponentInChildren<BoxCollider>();
 		}
 	
-		private void Update () {
-			_characterController.SimpleMove(Speed * Input.GetAxis("Vertical") * transform.forward);
+		private void FixedUpdate() {
+			GroundCheck();
+			var input = GetInput();
+			Debug.DrawRay(_rigidbody.position + new Vector3(1f, 0f, 0f), Quaternion.AngleAxis(90, Vector3.right) * new Vector3(input.x, input.y, 0f), Color.red, 0f, false);
+			Debug.DrawRay(_rigidbody.position, _rigidbody.velocity, Color.green, 0f, false);
+			if (_rigidbody.velocity.sqrMagnitude <
+			    MovementSettings.MaxSpeed * MovementSettings.MaxSpeed)
+				_rigidbody.AddForce(transform.forward * input.y * MovementSettings.MovementSpeed, ForceMode.Impulse);
+			_rigidbody.AddForce(transform.right * input.x * MovementSettings.MovementSpeed, ForceMode.VelocityChange);
+		}
+
+		private Vector2 GetInput() {
+			var input = new Vector2 {
+				x = Input.GetAxis("Horizontal"),
+				y = Input.GetAxis("Vertical")
+			};
+//			UpdateCurrentSpeed(input);
+			return input;
+		}
+
+//		private void UpdateCurrentSpeed(Vector2 input) {
+//			if (input == Vector2.zero) return;
+//			MovementSettings.CurrentTargetSpeed = Mathf.Clamp(MovementSettings.CurrentTargetSpeed + input.y, 0f, MovementSettings.MaxSpeed);
+//		}
+
+		private void GroundCheck() {
+			RaycastHit raycastHit;
+			if (Physics.BoxCast(_rigidbody.position, _boxCollider.size / 2f, -transform.up, out raycastHit, transform.rotation,
+				AdvancedSettings.GroundCheckDistance, AdvancedSettings.GroundCheckLayerMask.value,
+				QueryTriggerInteraction.Ignore)) {
+				_isGrounded = true;
+				_groundContactNormal = raycastHit.normal;
+			} else {
+				_isGrounded = false;
+				_groundContactNormal = Vector3.up;
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/Cobble/SpyHunter/Player/PlayerScroller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Cobble.SpyHunter.Player {
+    
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerScroller : MonoBehaviour {
+        
+        public float Speed = 5f;
+
+        public Vector3 ScrollDirection = Vector3.forward;
+
+        public bool RelativeDirection;
+
+        public ForceMode ScrollForceMode = ForceMode.Force;
+
+        [SerializeField]
+        private Rigidbody _rigidbody;
+        
+        private void Start() {
+            if (!_rigidbody)
+                _rigidbody = GetComponent<Rigidbody>();
+            ScrollDirection = ScrollDirection.normalized * Speed;
+        }
+
+        private void FixedUpdate() {
+            if (RelativeDirection)
+                _rigidbody.AddRelativeForce(ScrollDirection, ScrollForceMode);
+            else
+                _rigidbody.AddForce(ScrollDirection, ScrollForceMode);
+        }
+    }
+}
