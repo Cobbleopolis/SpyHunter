a278683b7443cbef2b753c02e63f4528873a1ce3
Cobbleopolis <cobbleopolis@gmail.com>
2018-04-26 13:52:38 -0400

    Added the ability to shoot bullets

---
+++ Assets/Scripts/Cobble/SpyHunter/Entity/BulletEntity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Cobble.SpyHunter.Entity {
+    [RequireComponent(typeof(Rigidbody))]
+    public class BulletEntity : MonoBehaviour {
+
+        public float MaxLife = 5f;
+
+        private void Start() {
+            Destroy(gameObject, MaxLife);
+        }
+
+        private void OnCollisionEnter(Collision other) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Cobble/SpyHunter/Player/BulletAbility.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using Cobble.Core.Managers;
+using UnityEngine;
+
+namespace Cobble.SpyHunter.Player {
+    public class BulletAbility : MonoBehaviour {
+
+        public GameObject BulletPrefab;
+        
+        public Vector3 BulletVelocity = Vector3.forward * 30f;
+
+        public Transform[] BulletSpawnLocations;
+
+        public float SpawnDelay = 0.1f;
+
+        public Collider PlayerCollider;
+
+        public Rigidbody PlayerRigidbody;
+
+        private void Update() {
+            if (!GameManager.IsPaused && Input.GetButtonDown("Fire1"))
+                StartCoroutine(FireBullet());
+        }
+
+        private IEnumerator FireBullet() {
+            while (!GameManager.IsPaused && Input.GetButton("Fire1")) {
+                foreach (var spawnTransform in BulletSpawnLocations)
+                    SpawnBullet(spawnTransform);
+                yield return new WaitForSeconds(SpawnDelay);
+            }
+        }
+
+        private void SpawnBullet(Transform spawnTransform) {
+            var bulletGameObject = Instantiate(BulletPrefab, spawnTransform.position,
+                spawnTransform.rotation);
+
+            var bulletCollider = bulletGameObject.GetComponent<Collider>();
+            if(bulletCollider)
+                Physics.IgnoreCollision(PlayerCollider, bulletCollider);
+                
+            var bulletRigidbody = bulletGameObject.GetComponent<Rigidbody>();
+            if (!bulletRigidbody) return;
+            var relativePlayerVel = PlayerRigidbody.velocity;
+            relativePlayerVel.y = 0;
+            bulletRigidbody.AddRelativeForce(BulletVelocity + relativePlayerVel, ForceMode.VelocityChange);
+        }
+    }
+}
