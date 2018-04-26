6558075e7787ccab0113345ce70c994dd56065f3
Cobbleopolis <cobbleopolis@gmail.com>
2018-04-26 02:33:35 -0400

    Added basic car spawning

---
+++ Assets/Scripts/Cobble/SpyHunter/Entity/VehicleDespawner.cs
@@ -0,0 +1,12 @@
+﻿using Cobble.SpyHunter.AI;
+using UnityEngine;
+
+namespace Cobble.SpyHunter.Entity {
+    public class VehicleDespawner : MonoBehaviour {
+        private void OnTriggerEnter(Collider other) {
+            var carAi = other.gameObject.GetComponentInParent<CarAi>(); 
+            if (carAi)
+                Destroy(carAi.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Cobble/SpyHunter/Entity/VehicleSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Cobble.Core.Managers;
+using UnityEngine;
+
+namespace Cobble.SpyHunter.Entity {
+    public class VehicleSpawner : MonoBehaviour {
+
+        public Collider SpawnArea;
+
+        public float SpawnTimer = 5f;
+
+        public GameObject[] VehiclePrefabs;
+
+        private Bounds _bounds;
+
+        private void Start() {
+            if (!SpawnArea)
+                SpawnArea = GetComponent<Collider>();
+            if (SpawnArea)
+                _bounds = SpawnArea.bounds;
+            StartCoroutine(SpawnHandler());
+        }
+
+        private IEnumerator SpawnHandler() {
+            while (_bounds != null) {
+                yield return new WaitForSeconds(SpawnTimer);
+                _bounds = SpawnArea.bounds;
+                if (!GameManager.IsPaused) {
+                    var spawnLoaction = new Vector3(
+                        Random.Range(_bounds.min.x, _bounds.max.x),
+                        Random.Range(_bounds.min.y, _bounds.max.y),
+                        Random.Range(_bounds.min.z, _bounds.max.z)
+                    );
+                    SpawnVehicle(spawnLoaction);
+                }
+            }
+        }
+
+        private void SpawnVehicle(Vector3 spawnLocation) {
+            Instantiate(VehiclePrefabs[Random.Range(0, VehiclePrefabs.Length)], spawnLocation, Quaternion.identity);
+        }
+    }
+}
