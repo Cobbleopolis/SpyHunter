ff061b73252cf0771c20fc61ffd6db27751d07c4
Cobbleopolis <cobbleopolis@gmail.com>
2018-04-29 11:56:31 -0400

    Player lives now display after timer ends

--- Assets/Scripts/Cobble/SpyHunter/Player/LifeHandler.cs
+++ Assets/Scripts/Cobble/SpyHunter/Player/LifeHandler.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 
 namespace Cobble.SpyHunter.Player {
     public class LifeHandler : MonoBehaviour {
-
         [Tooltip("The amount of time at once the player starts moving that the player has unlimited lives.")]
         public int LeadInCount = 1000;
 
         public int DecrementDelay = 10;
 
-        [Tooltip("The number of extra cars that the player has.")]
-        [SerializeField]
+        public int MaxLives = 5;
+
+        [Tooltip("The number of extra cars that the player has.")] [SerializeField]
         private int _extraLives;
 
         private int _delayCount;
-        
+
         public int ExtraLives {
             get { return _extraLives; }
         }
@@ -25,23 +25,21 @@ namespace Cobble.SpyHunter.Player {
                 LeadInCount--;
                 _delayCount = 0;
             }
+
             _delayCount++;
-//            if (LeadInTimeSeconds > 0)
-//                LeadInTimeSeconds -= Time.deltaTime;
         }
 
         public void AddLives(int amount = 1) {
-            _extraLives += amount;
+            _extraLives = Mathf.Clamp(_extraLives + amount, 0, MaxLives);
         }
 
         public void RemoveLife(int amount = 1) {
-            if (LeadInCount > 0)
-                _extraLives -= amount;
+            if (LeadInCount <= 0)
+                _extraLives = Mathf.Clamp(_extraLives - amount, 0, MaxLives);
         }
 
         public bool HasExtraLife() {
             return LeadInCount > 0 || ExtraLives > 0;
         }
-        
     }
 }
--- Assets/Scripts/Cobble/SpyHunter/Ui/LifePanelHud.cs
+++ Assets/Scripts/Cobble/SpyHunter/Ui/LifePanelHud.cs
@@ -8,6 +8,10 @@ namespace Cobble.SpyHunter.Ui {
 
         public Text TimerText;
 
+        public GameObject LifeContainer;
+
+        public GameObject LifeIconPrefab;
+
         private void Start() {
             if (!PlayerLifeHandler)
                 PlayerLifeHandler = GameObject.FindWithTag("Player").GetComponent<LifeHandler>();
@@ -15,10 +19,23 @@ namespace Cobble.SpyHunter.Ui {
 
         private void Update() {
             if (PlayerLifeHandler.LeadInCount > 0) {
+                if (LifeContainer.activeInHierarchy)
+                    LifeContainer.SetActive(false);
                 TimerText.text = PlayerLifeHandler.LeadInCount.ToString();
             } else {
-                if (TimerText.gameObject.activeInHierarchy)
+                if (TimerText.gameObject.activeInHierarchy) {
                     TimerText.gameObject.SetActive(false);
+                    LifeContainer.SetActive(true);
+                }
+
+                var childCount = LifeContainer.transform.childCount;
+                while (childCount < PlayerLifeHandler.ExtraLives) {
+                    Instantiate(LifeIconPrefab, LifeContainer.transform);
+                    childCount++;
+                }
+
+                while (childCount > PlayerLifeHandler.ExtraLives)
+                    Destroy(LifeContainer.transform.GetChild(--childCount).gameObject);
             }
         }
     }
